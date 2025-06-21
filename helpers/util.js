5eb02babe34d5473d71e98730bf8e44134464c60
papa-zola-rgb <himmiassyarfi25@gmail.com>
2025-06-21 11:24:19 +0700

    commit

--- helpers/util.js
+++ helpers/util.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const pool = new Pool(
   process.env.DATABASE_URL
     ? {
-        connectionString: process.env.DATABASE_URL,
+        connectionString: process.env.DB_URL,
         ssl: { rejectUnauthorized: false },
       }
     : {
