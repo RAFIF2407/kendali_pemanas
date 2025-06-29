c16389e36b6e368fc8c9c8dd4611defdbb212631
GalangBees <214851817+GalangBees@users.noreply.github.com>
2025-06-29 17:08:29 +0700

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
 //koneksi ke server socket.io//
-var socket = io("https://trainer-pid.up.railway.app/", {
+var socket = io("https://pid-trainer.up.railway.app/", {
   query: { nim: String(window.myNim).trim() },
 });
 
