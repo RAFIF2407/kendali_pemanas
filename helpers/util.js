259507095e246bab7413a8f4bd9cdb947021c677
papa-zola-rgb <himmiassyarfi25@gmail.com>
2025-06-29 13:06:34 +0700

    YES

--- helpers/util.js
+++ helpers/util.js
@@ -3,17 +3,17 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool(
-  process.env.DATABASE_URL
+  process.env.DB_URL
     ? {
         connectionString: process.env.DB_URL,
         ssl: { rejectUnauthorized: false },
       }
     : {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
+        host: process.env.PGHOST,
+        port: process.env.PGPORT,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGNAME,
       }
 );
 
