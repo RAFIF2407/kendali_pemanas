e317f99c42727b3b16435390d0d14df67391d82d
papa-zola-rgb <himmiassyarfi25@gmail.com>
2025-07-10 08:01:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(
       // secure: false, // false untuk developer/lokal | true untuk production/online
       secure: process.env.NODE_ENV === "production", // AKTIFKAN Cookie hanya dikirim di HTTPS jika dalam produksi
       sameSite: "lax",
-      maxAge: 24 * 60 * 60 * 1000),
+      maxAge: "8 * 60 * 60 * 1000",
     },
   })
 );
